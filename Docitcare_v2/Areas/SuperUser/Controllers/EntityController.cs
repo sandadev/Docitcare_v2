7163664fdcbf48d6e1a3494e525afd20386ca3e4
Mohit Kothiyal <v-mohkot@microsoft.com>
2021-09-14 17:31:31 +0530

    Test the git commit

--- Docitcare_v2/Areas/SuperUser/Controllers/EntityController.cs
+++ Docitcare_v2/Areas/SuperUser/Controllers/EntityController.cs
@@ -16,6 +16,7 @@ namespace Docitcare_v2.Areas.SuperUser.Controllers
             _unitOfWork = unitOfWork;
         }
         // GET: SuperUser/Entity
+        //testgit
         public ActionResult Index()
         {
            // _unitOfWork.Entities.GetAll();
